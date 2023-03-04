aa4838d4aecf24e24200e3f67c94cf68c6dcc5c9
lord-of-cinder-2000 <vuvanquyet17102@gmail.com>
2023-03-04 07:04:38 +0700

    Done payment-module 1

--- quasar-game-streaming-ts/src/pages/TestPage.vue
+++ quasar-game-streaming-ts/src/pages/TestPage.vue
@@ -1,18 +1,49 @@
 <template>
-	<q-page padding>
-		<TopupPopup />
+	<q-page
+		id="iframe-container"
+		style="width: 800px; height: 80vh"
+		class="dimmed flex flex-center"
+	>
+		<!-- <iframe
+			ref="iframe"
+			id="xsolla-iframe"
+			class="fit no-margin no-border"
+			:src="xsollaPaystationUrl"
+			frameborder="0"
+		/> -->
+		<div>
+			<img
+				style="width: 100px; mix-blend-mode: lighten"
+				src="https://img.trovo.live/imgupload/shop/20220824_mzo0rt1v8d.webp"
+			/>
+		</div>
 	</q-page>
 </template>
 
-<script setup lang="ts">
-import { ref, defineAsyncComponent } from "vue";
-const DonateCard = defineAsyncComponent(
-	() => import("components/payments/DonatePopup.vue")
+<script setup>
+import { ref, defineAsyncComponent, computed, onMounted, nextTick } from "vue";
+const XsollaIframe = defineAsyncComponent(() =>
+	import("components/payments/XsollaIframe.vue")
 );
-
-const TopupPopup = defineAsyncComponent(
-	() => import("components/payments/TopupPopup.vue")
+const xsollaPaystationUrl = computed(
+	() =>
+		process.env.XSOLLA_PAYSTATION_URL +
+		"txqfOhKluqqSaAbrAG7iMznKDhhdIkqh_lc_vi_bg_FFFFFF_tb_3D46F5"
 );
+const show = ref(true);
+const openXsolla = ref(false);
+// var iframe = document.getElementById("xsolla-iframe");
+// var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+// var element = innerDoc.getElementById("my-element");
+// element.style.color = "red";
+// element.style.fontWeight = "bold";
+const iframe = ref(null);
+onMounted(() => {});
 </script>
 
-<style scoped></style>
+<style>
+#xsolla-iframe::-webkit-scrollbar {
+	width: 1px;
+	height: 1px;
+}
+</style>
